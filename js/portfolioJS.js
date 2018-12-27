2b80695f856279dd23b34e351bafb25b0c2923ec
Ryan <werner.ryan18@gmail.com>
2018-12-27 13:22:55 -0800

    added files

---
+++ js/portfolioJS.js
@@ -0,0 +1,19 @@
+function getAbout(){
+    document.getElementById("JSobj").innerHTML=(
+    '<p>The part where I would actually talk about me.</p>'+
+    '<p>Some of my hobbies</p>'+
+    '<ul class="icon-bar">'+
+    '<a class="icon" href=""><i class="fab fa-github"></i></a>'+
+    '<a class="icon" href=""><i class="fab fa-linkedin"></i></a>'+
+    '<a class="icon" href=""><i class="fab fa-instagram"></i></a>'+
+    '</ul>');
+}
+function getHtmlCss(){
+    document.getElementById("JSobj").innerHTML="Solid foundation and understanding of HTML and CSS.";
+}
+function getJS(){
+    document.getElementById("JSobj").innerHTML=("Solid foundation with Javascript and fluent understanding of Object Orientated Programming.");
+}
+function getUX(){
+    document.getElementById("JSobj").innerHTML="Talentful Eye for the minute design of proper UX design.";
+}
