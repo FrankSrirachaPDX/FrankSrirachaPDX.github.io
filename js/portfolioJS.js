aacdc4a4f7f8b1c8dcfb8a38369a17b13e93e05d
Ryan <werner.ryan18@gmail.com>
2018-12-28 14:06:34 -0800

    Implemented skills section

--- js/portfolioJS.js
+++ js/portfolioJS.js
@@ -9,7 +9,22 @@ function getAbout(){
     '</ul>');
 }
 function getHtmlCss(){
-    document.getElementById("JSobj").innerHTML=('[Text Placeholder]');
+    document.getElementById("JSobj").innerHTML=(
+'<div class="progress">'+
+'<div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" style="width: 90%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="90">HTML</div>'+
+'</div>'+
+'<div class="progress">'+
+'<div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" style="width: 90%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="90">CSS</div>'+
+'</div>'+
+'<div class="progress">'+
+'<div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="75">JavaScript</div>'+
+'</div>'+
+'<div class="progress">'+
+'<div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" style="width: 70%" aria-valuenow="70" aria-valuemin="0" aria-valuemax="70">Python</div>'+
+'</div>'+
+'<div class="progress">'+
+'<div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" style="width: 40%" aria-valuenow="40" aria-valuemin="0" aria-valuemax="40">UX</div>'+
+'</div>');
 }
 function getJS(){
     document.getElementById("JSobj").innerHTML=('[Text Placeholder]');
